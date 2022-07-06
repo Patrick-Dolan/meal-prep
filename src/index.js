8737161c3b1ed27313a38cd289f5bcf66a12f1dc
Patrick Dolan <dolanp1992@gmail.com>
2022-07-05 20:04:43 -0700

    setup redux store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from "./reducers/recipes-reducer";
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: reducer
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
