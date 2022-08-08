d8e760d10bd4bd21a8dee15f7c069464a0eee32c
Patrick Dolan <dolanp1992@gmail.com>
2022-08-08 12:30:09 -0700

    wrap app component with browser router

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import { configureStore } from '@reduxjs/toolkit';
-import reducer from "./reducers/recipes-reducer";
 import { Provider } from 'react-redux';
+import reducer from "./reducers/recipes-reducer";
+import { BrowserRouter } from 'react-router-dom';
 
 const store = configureStore({
   reducer: reducer
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
