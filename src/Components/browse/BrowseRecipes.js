99d2fe68c1f22a2790fda3f185f93b3494fb9f13
Patrick Dolan <dolanp1992@gmail.com>
2022-09-05 14:19:33 -0700

    refactor: move api call into component     refactor: change state management to useState instead of redux

--- src/Components/browse/BrowseRecipes.js
+++ src/Components/browse/BrowseRecipes.js
@@ -1,24 +1,61 @@
 //import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { makeRecipeApiCall } from "../../actions/index";
+import { useState } from "react";
 import { Button, Grid, Typography } from "@mui/material";
 import RecipeCard from "../my-recipes/RecipeCard";
 import RecipeCardSkeleton from "../my-recipes/RecipeCardSkeleton";
 
-const BrowseRecipes = () => {
-  const dispatch = useDispatch();
-  const error = useSelector(state => state.error);
-  const isLoading = useSelector(state => state.isLoading);
-  const recipes = useSelector(state => state.recipes);
+// API call header details
+const options = {
+  method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': `${process.env.REACT_APP_TASTY_API_KEY}`,
+		'X-RapidAPI-Host': `${process.env.REACT_APP_TASTY_API_HOST}`
+	}
+}
 
+const BrowseRecipes = () => {
+  const [apiRecipes, setApiRecipes] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  
   const skeletonCount = 6;
   
+  console.log("Api recipes: ", apiRecipes)
   // useEffect(() => {
   //   dispatch(makeRecipeApiCall());
   // }, [dispatch]);
 
+  const sortRecipes = (recipes) => {
+    const sortedRecipes = [];
+    recipes.forEach(recipe => {
+      if (recipe.description.length !== 0) {
+        sortedRecipes.push(recipe);
+      }
+    });
+    return sortedRecipes;
+  }
+  
+  const makeRecipeApiCall = () => {
+    try {
+      fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=100&tags=lunch', options)
+        .then(response => response.json())
+        .then(jsonifiedReponse => {
+          setApiRecipes(sortRecipes(jsonifiedReponse.results))
+          setIsLoading(false);
+        })
+        .catch(error => {
+          setError(error);
+        });
+    } catch (error) {
+      
+      console.log("Api call failed: ", error);
+    }
+  }
+  
+
   const handleRecipeButtonClick = () => {
-    dispatch(makeRecipeApiCall());
+    setIsLoading(true);
+    makeRecipeApiCall();
   }
 
   if (error) {
@@ -43,13 +80,16 @@ const BrowseRecipes = () => {
         <Typography variant="h3">Tasty Recipes</Typography>
         <Button variant="contained" onClick={handleRecipeButtonClick}>Load Recipes</Button>
         <hr />
-        <Grid container spacing={2}>
-          {recipes.map((recipe) =>
-            <Grid item xs={12} sm={6} md={4} key={recipe.id}>
-              <RecipeCard {...recipe} />
-            </Grid>
-          )}
-        </Grid>
+        {(apiRecipes) ? (
+          <Grid container spacing={2}>
+            {apiRecipes.map((apiRecipe) =>
+              <Grid item xs={12} sm={6} md={4} key={apiRecipe.id}>
+                <RecipeCard {...apiRecipe} />
+              </Grid>
+            )}
+          </Grid>
+        ) : (null)
+        }
       </>
     );
   }
