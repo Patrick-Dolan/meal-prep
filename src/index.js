1574ed1d416dd32ed7925c5322a31f525878af17
Patrick Dolan <dolanp1992@gmail.com>
2022-09-05 14:17:32 -0700

    refactor: remove redux configurations

--- src/index.js
+++ src/index.js
@@ -2,25 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-import reducer from "./reducers/recipes-reducer";
 import { BrowserRouter } from 'react-router-dom';
-
-const store = configureStore({
-  reducer: reducer
-})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 reportWebVitals();
