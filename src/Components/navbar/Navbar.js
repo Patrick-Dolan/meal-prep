cf8db86b2726b8bb908051e3834670cbb1126ccb
Patrick Dolan <dolanp1992@gmail.com>
2022-09-07 11:43:34 -0700

    update: change maxWidth to lg from xl

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -62,7 +62,7 @@ const Navbar = () => {
 
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
+      <Container maxWidth="lg">
         <Toolbar disableGutters>
           <TakeoutDining sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
