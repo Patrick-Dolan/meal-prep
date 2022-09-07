d1683fe56715fc6386321b98002ce021d299b1ff
Patrick Dolan <dolanp1992@gmail.com>
2022-09-07 14:52:18 -0700

    update: add account component and create route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Navbar from "./Components/navbar/Navbar";
 import BrowseRecipes from "./Components/browse/BrowseRecipes";
-import { Route, Routes } from "react-router";
 import MyRecipes from "./Components/my-recipes/MyRecipes";
 import MealPlans from "./Components/meal-plans/MealPlans";
+import Account from "./Components/account/Account";
+import { Route, Routes } from "react-router";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme/index";
 import { AuthProvider } from "./Contexts/AuthContext";
@@ -19,6 +20,7 @@ function App() {
           <Route path="Browse" element={<BrowseRecipes />} />
           <Route path="My%20Recipes" element={<MyRecipes />} />
           <Route path="Meal%20Plans" element={<MealPlans />} />
+          <Route path="account" element={<Account />} />
         </Routes>
       </ThemeProvider>
     </AuthProvider>
