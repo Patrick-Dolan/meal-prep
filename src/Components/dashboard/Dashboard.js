dd7db368d4a100ae064b24579938ef24d12f75fd
Patrick Dolan <dolanp1992@gmail.com>
2022-09-07 14:48:15 -0700

    feature: add dashboard component

---
+++ src/Components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import { Container } from "@mui/system"
+
+const Dashboard = () => {
+  return (
+    <Container>
+      {/* TODO add dashboard */}
+    </Container>
+  )
+}
+
+export default Dashboard;
