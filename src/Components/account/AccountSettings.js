c2bcabf595bc42590cd90da242d224204670b27f
Patrick Dolan <dolanp1992@gmail.com>
2022-09-09 18:15:11 -0700

    update: add email and name to account information

--- src/Components/account/AccountSettings.js
+++ src/Components/account/AccountSettings.js
@@ -1,11 +1,16 @@
 import { Container } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Divider, Typography } from "@mui/material";
+import { UserAuth } from "../../Contexts/AuthContext";
 
 const AccountSettings = () => {
+  const { user } = UserAuth();
+
   return (
     <Container>
-      <Typography variant="h5">Account settings component</Typography>
-      <br />
+      <Typography variant="h5">Account information</Typography>
+      <Divider />
+      <Typography variant="h6">Email:</Typography><Typography variant="body2">{user?.email}</Typography>
+      <Typography variant="h6">Name:</Typography><Typography variant="body2">{user?.firstName} {user?.lastName}</Typography>
     </Container>
   )
 }
