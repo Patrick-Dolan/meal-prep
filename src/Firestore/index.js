e64f6268ea9cd271b3f112dfab159bf42e737222
Patrick Dolan <dolanp1992@gmail.com>
2022-09-09 18:07:41 -0700

    feature: add firestore create, update, and get functionality for user db collection

---
+++ src/Firestore/index.js
@@ -0,0 +1,27 @@
+import { db } from "../firebase";
+import { doc, setDoc, serverTimestamp, getDoc } from "firebase/firestore";
+
+// Add or Update takes in user from auth and userDetails object
+export const updateUserDBEntry = async (user, userDetails) => {
+  // Create Account doc for new user with same user id
+  const docRef = doc(db, "users", user.user.uid );
+  const payload = {
+    ...userDetails,
+    createdAt: serverTimestamp()
+  }
+  await setDoc(docRef, payload);
+}
+
+// Get user data
+export const getUserData = async (user) => {
+  const docRef = doc(db, "users", user.uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    // console.log("Document data:", docSnap.data());
+    // console.log("Doc Data: ", userData)
+    return docSnap.data();
+  } else {
+    console.log("No document found.");
+  }
+}
