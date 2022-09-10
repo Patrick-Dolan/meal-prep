328c8d35729127fb252a9049e8acb5aa832361ca
Patrick Dolan <dolanp1992@gmail.com>
2022-09-09 18:09:40 -0700

    update: add firestore user data to user object for auth context

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 import { auth } from "../firebase";
+import { getUserData } from "../Firestore";
 
 // Create context
 export const AuthContext = createContext();
@@ -9,7 +10,8 @@ export const AuthContext = createContext();
 export const AuthProvider = ({children}) => {
 
   const [user, setUser] = useState({});
-
+  const [hasAddedDbData, setHasAddedDbData] = useState(false);
+  
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -20,6 +22,24 @@ export const AuthProvider = ({children}) => {
     }
   }, []);
 
+  useEffect(() => {
+    const getAndMergeUserDbData = async () => {
+      const dbData = await getUserData(user);
+      const updatedUser = {
+        ...user,
+        ...dbData
+      };
+      setUser(updatedUser);
+    };
+
+    if (user?.uid && !hasAddedDbData) {
+      getAndMergeUserDbData();
+      setHasAddedDbData(true);
+    }
+  }, [user, hasAddedDbData]);
+
+  
+
   const registerUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   }
@@ -29,9 +49,10 @@ export const AuthProvider = ({children}) => {
   }
 
   const logout = async () => {
+    setHasAddedDbData(false);
     return signOut(auth);
   }
-  
+
   return (
     <AuthContext.Provider value={{ user, registerUser, signIn, logout }}>
       {children}
