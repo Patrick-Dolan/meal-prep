290bb64f6e9d53e32519c6e36d134d917d27078f
Patrick Dolan <dolanp1992@gmail.com>
2022-09-12 18:24:05 -0700

    update: add comments and change setdoc to merge new data

--- src/firebasefunctions/index.js
+++ src/firebasefunctions/index.js
@@ -1,15 +1,17 @@
 import { db } from "../firebase";
 import { doc, setDoc, serverTimestamp, getDoc } from "firebase/firestore";
 
+// ==================== Firestore Functions ====================
+
 // Add or Update takes in user from auth and userDetails object
 export const updateUserDBEntry = async (user, userDetails) => {
   // Create Account doc for new user with same user id
-  const docRef = doc(db, "users", user.user.uid );
+  const docRef = doc(db, "users", user.uid );
   const payload = {
     ...userDetails,
-    createdAt: serverTimestamp()
+    createdAt: user.createdAt || serverTimestamp()
   }
-  await setDoc(docRef, payload);
+  await setDoc(docRef, payload, { merge: true });
 }
 
 // Get User data
