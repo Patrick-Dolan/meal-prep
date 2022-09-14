1f4c87f0853784f25239e6bb99544c9ef00a66ee
Patrick Dolan <dolanp1992@gmail.com>
2022-09-13 21:35:48 -0700

    feature: add profilecard component to display fun and useful user information

---
+++ src/Components/account/ProfileCard.js
@@ -0,0 +1,37 @@
+import { Box, Card, CardContent, Typography, Avatar, Button, Divider, Paper} from "@mui/material"
+import { UserAuth } from "../../Contexts/AuthContext";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const ProfileCard = (props) => {
+  const { user } = UserAuth();
+  const { toggleProfile } = props;
+
+  return (
+    <Box display="inline-block" sx={{width: 1}}>
+      <Paper elevation={4}>
+        <Card>
+          <CardContent>
+            <Avatar 
+              sx={{height: "5em", width: "5em", m: "auto", mb: ".5em"}} 
+              src={user.photoURL}
+            />
+            <Typography variant="subtitle1" align="center">{user?.firstName} {user?.lastName}</Typography>
+            <Divider />
+          </CardContent>
+          {(toggleProfile) ? (
+            <Box sx={{mx: ".5em", mb: ".5em"}}>
+              <Button onClick={() => toggleProfile(true)} startIcon={<AccountCircleIcon />} size="small" variant="outlined" fullWidth>Account</Button>
+              <br />
+              <Button onClick={() => toggleProfile(false)} startIcon={<SettingsIcon />} size="small" variant="outlined" fullWidth>Settings</Button>
+            </Box>
+          ) : (
+            null
+          )}
+        </Card>
+      </Paper>
+    </Box>
+  )
+}
+
+export default ProfileCard;
