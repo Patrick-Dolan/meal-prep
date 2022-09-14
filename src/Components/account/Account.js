f4be87bbd76949ef1bdbb4bdaaa5fd20909fc94d
Patrick Dolan <dolanp1992@gmail.com>
2022-09-13 21:41:41 -0700

    refactor: set toggle system to show account settings or profile information based on card buttons    update: add profile card and styling

--- src/Components/account/Account.js
+++ src/Components/account/Account.js
@@ -1,15 +1,30 @@
-import { Typography } from "@mui/material";
-import { Container } from "@mui/system"
+import { Divider, Grid, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import { useState } from "react";
+import ProfileCard from "./ProfileCard";
+import ProfileInformation from "./ProfileInformation";
 import AccountSettings from "./AccountSettings";
-import AccountUpdate from "./AccountUpdate";
 
 const Account = () => {
+  const [toggleProfile, setToggleProfile] = useState(true);
+
   return (
     <Container sx={{mt: "1em"}}>
       <Typography variant="h5">Account</Typography>
+      <Divider />
       <br />
-      <AccountSettings />
-      <AccountUpdate />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4} md={3} textAlign="center">
+          <ProfileCard toggleProfile={setToggleProfile} />
+        </Grid>
+        <Grid item xs={12} sm={8} md={9}>
+          {(toggleProfile) ? (
+            <ProfileInformation />
+          ) : (
+            <AccountSettings />
+          )}
+        </Grid>
+      </Grid>
     </Container>
   )
 }
