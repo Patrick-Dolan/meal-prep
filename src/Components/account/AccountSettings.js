38679304383d31e13717e7f763a0443483c8ecd1
Patrick Dolan <dolanp1992@gmail.com>
2022-09-14 11:04:30 -0700

    refactor: move photo updating from accountsettings component to profileinformation     update: add edit toggle buttons and link to switch between data view and forms       update: add title

--- src/Components/account/AccountSettings.js
+++ src/Components/account/AccountSettings.js
@@ -1,139 +1,27 @@
-import { Avatar, Button, Dialog, DialogContent, DialogTitle, DialogActions, DialogContentText, Divider, Grid, TextField, Typography, Paper } from "@mui/material";
+import { Button, Divider, Grid, TextField, Typography, Paper } from "@mui/material";
 import { Container } from "@mui/system";
-import EditIcon from '@mui/icons-material/Edit';
 import { useState } from "react";
 import { UserAuth } from "../../Contexts/AuthContext";
-import { ref, uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage";
-import { db, storage } from "../../firebase";
-import { updateUserDBEntry } from "../../firebasefunctions"
-import { deleteField, doc, updateDoc } from "firebase/firestore";
+import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
 
 const AccountSettings = () => {
-  const { user, setUser } = UserAuth();
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const uploadProfilePicture = async (file) => {
-    // Delete old pfp if it exists
-    if (user.photoPath != null && user.photoURL != null) {
-      deleteProfilePicture();
-      deleteProfilePictureUserFields();
-    }
-
-    // Create filepath for new profile picture
-    let filePath = `media/users/${user.uid}/profilepicture/${file[0].name}`;
-
-    const storageRef = ref(storage, filePath);
-    const uploadTask = uploadBytesResumable(storageRef, file[0]);
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on('state_changed', 
-    (snapshot) => {
-      // Observe state change events such as progress, pause, and resume
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case 'paused':
-          console.log('Upload is paused');
-          break;
-        case 'running':
-          console.log('Upload is running');
-          break;
-        default:
-          console.log("Error");
-      }
-    }, 
-    (error) => {
-      // Handle unsuccessful uploads
-      console.log("Upload Error: ", error.message)
-    }, 
-    () => {
-      // Handle successful uploads on complete
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        console.log('File available at', downloadURL);
-        try {
-          // Setup updated user db record and update it
-          let payload = {
-            photoURL: downloadURL, 
-            photoPath: filePath
-          }
-          updateUserDBEntry(user, payload)
-          // Set state so site will rerender immediately
-          setUser({...user, ...payload})
-        } catch (e) {
-          console.log("Update Error: ", e.message)
-        }
-      });
-    }
-    );
-  }
-
-  const deleteProfilePictureUserFields = async () => {
-    try {
-      const userDbEntry = doc(db, "users", user.uid);
-  
-      await updateDoc(userDbEntry, {
-        photoPath: deleteField(),
-        photoURL: deleteField()
-      })
-
-      const updatedUser = {
-        ...user,
-        photoPath: null,
-        photoURL: null
-      }
-      setUser({...updatedUser});
-    } catch (error) {
-      console.log("Database entry deletion error: ", error.message);
-    }
-  }
-
-  const deleteProfilePicture = async () => {
-    try {
-      const storageRef = ref(storage, user.photoPath);
-
-      // Delete the file
-      deleteObject(storageRef).then(() => {
-        // TODO add success snackbar
-        console.log("Picture deletion successful")
-      }).catch((error) => {
-        // TODO add success snackbar
-        console.log("Picture deletion failed: ", error.message)
-      });
-    } catch (error) {
-      console.log("Delete file error: ", error.message);
-    }
-  }
-
-  const handleFileUpload = (e) => {
-    if (user.uid) {
-      uploadProfilePicture(e.target.files);
-    }
-  }
-
-  const handleFileDelete = () => {
-    deleteProfilePicture();
-    deleteProfilePictureUserFields();
-    handleClose();
-  }
+  const { user } = UserAuth();
+  const [ editEmailAndPassword, setEditEmailAndPassword ] = useState(false);
 
   const handleEditClick = () => {
-    alert("Account Edit clicked!");
+    setEditEmailAndPassword(true);
+    if (editEmailAndPassword) {setEditEmailAndPassword(false)}
+  }
+
+  const handleSubmitAccountChanges = () => {
+    alert("Account changes clicked!")
   }
 
   return (
     <Paper elevation={5}>
       <Container maxWidth="lg">
+      <Divider sx={{pt: "1em"}}><Typography variant="h5" fontWeight="500">Account Settings</Typography></Divider>
         <Grid container justifyContent={"space-between"} alignItems="center">
           <Grid item>
             <Typography variant="h6" sx={{pt: ".5em"}}>Settings</Typography>
@@ -155,7 +43,21 @@ const AccountSettings = () => {
             <Typography variant="body2">Email</Typography>
           </Grid>
           <Grid item xs={9} sm={9} md={9}>
-            <Typography variant="body2">{user?.email}</Typography>
+            {(editEmailAndPassword) ? (
+              <TextField
+                margin="dense"
+                id="email"
+                label="Email"
+                type="email"
+                variant="outlined"
+                size="small"
+                defaultValue={user.email}
+                fullWidth
+                required
+              />
+            ) : (
+              <Typography variant="body2">{user?.email}</Typography>
+            )}
           </Grid>
         </Grid>
         <Grid container sx={{pb: "4em"}}>
@@ -164,100 +66,35 @@ const AccountSettings = () => {
           </Grid>
           <Grid item xs={9} sm={9} md={9}>
             {/* TODO setup system to count password length or figure out how to show number of characters in password */}
-            <Typography variant="body2">*********</Typography>
-          </Grid>
-        </Grid>
-        {/* Profile Photo Section */}
-        <Grid container direction="row" alignItems={"center"} sx={{mt: "1em"}}>
-          <Grid item>
-            <Avatar 
-              sx={{height: "5em", width: "5em"}} 
-              src={user.photoURL}
-            />
-          </Grid>
-          <Grid item sx={{ml: "1em"}}>
-            <Button sx={{minWidth: "12em"}} variant="contained" component="label">
-              Change Photo
-              <input hidden accept="image/*" type="file" onChange={handleFileUpload}/>
-            </Button>
-            <br />
-            <Button sx={{minWidth: "12em", mt: ".5em"}} onClick={handleClickOpen} variant="outlined" color="error">
-              Delete Photo
-            </Button>
-          </Grid>
-        </Grid>
-        {/* User Data Section */}
-        <form>
-          <Grid container>
-            <Grid item>
-              
-            </Grid>
-            <Grid item>
-              <TextField
-                margin="dense"
-                id="email"
-                label="Email"
-                type="email"
-                variant="outlined"
-                defaultValue={user.email}
-                fullWidth
-                required
-              />
-            </Grid>
-            <Grid item>
+            {(editEmailAndPassword) ? (
               <TextField
                 margin="dense"
                 id="password"
                 label="Password"
                 type="password"
                 variant="outlined"
+                size="small"
                 fullWidth
                 required
               />
-            </Grid>
-            <Grid item>
-              <TextField
-                margin="dense"
-                id="favorite-food"
-                label="Favorite Food"
-                type="text"
-                variant="outlined"
-                fullWidth
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                margin="dense"
-                id="bio"
-                label="Bio"
-                type="text"
-                variant="outlined"
-                fullWidth
-              />
-            </Grid>
+            ) : (
+              <Typography variant="body2">*********</Typography>
+            )}
           </Grid>
-        </form>
-        {/* DELETE PROFILE PICTURE DIALOG */}
-        <Dialog
-        open={open}
-        onClose={handleClose}
-        >
-        <DialogTitle>
-          {"Delete user profile photo?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Once you delete your profile photo you won't be able to use the same photo unless you re-upload it.
-            Are you sure you wish to delete your profile photo?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-          <Button onClick={handleFileDelete}>
-            Delete
-          </Button>
-        </DialogActions>
-        </Dialog>
+        </Grid>
+        {(editEmailAndPassword) ? (
+            <Button
+              onClick={handleSubmitAccountChanges}
+              startIcon={<SaveIcon />}
+              variant="contained"
+              size="small"
+              sx={{mb: "1.5em"}}
+            >
+              Save changes
+            </Button>
+          ) : (
+            null
+          )}
       </Container>
     </Paper>
   )
--- src/Components/account/ProfileInformation.js
+++ src/Components/account/ProfileInformation.js
@@ -1,18 +1,149 @@
 import { Container } from "@mui/system";
-import { Button, Divider, Grid, Typography, Paper, TextField } from "@mui/material";
+import { Button, Divider, Grid, Typography, Paper, TextField, Avatar, Dialog, DialogContent, DialogTitle, DialogActions, DialogContentText, Link } from "@mui/material";
 import { UserAuth } from "../../Contexts/AuthContext";
+import { ref, uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage";
+import { deleteField, doc, updateDoc } from "firebase/firestore";
+import { updateUserDBEntry } from "../../firebasefunctions"
+import { db, storage } from "../../firebase";
 import { useState } from "react";
-// import { useTheme } from "@mui/material/styles";
 import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
+// import { useTheme } from "@mui/material/styles";
 
 const ProfileInformation = () => {
-  const { user } = UserAuth();
-  const [ editProfile, setEditProfile ] = useState(false);
+  const { user, setUser } = UserAuth();
+  const [ editProfileInformation, setEditProfileInformation ] = useState(false);
+  const [ editProfilePhoto, setEditProfilePhoto ] = useState(false);
+  const [open, setOpen] = useState(false);
   // const theme = useTheme();
 
-  const handleEditClick = () => {
-    setEditProfile(true);
-    if (editProfile) {setEditProfile(false)}
+  const handleEditProfileInformationClick = () => {
+    setEditProfileInformation(true);
+    if (editProfileInformation) {setEditProfileInformation(false)}
+  }
+
+  const handleEditProfileClick = () => {
+    setEditProfilePhoto(true);
+    if (editProfilePhoto) {setEditProfilePhoto(false)}
+  }
+
+  const handleSubmitProfileChanges = () => {
+    alert("Update Clicked!")
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const uploadProfilePicture = async (file) => {
+    // Delete old pfp if it exists
+    if (user.photoPath != null && user.photoURL != null) {
+      deleteProfilePicture();
+      deleteProfilePictureUserFields();
+    }
+
+    // Create filepath for new profile picture
+    let filePath = `media/users/${user.uid}/profilepicture/${file[0].name}`;
+
+    const storageRef = ref(storage, filePath);
+    const uploadTask = uploadBytesResumable(storageRef, file[0]);
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on('state_changed', 
+    (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+        default:
+          console.log("Error");
+      }
+    }, 
+    (error) => {
+      // Handle unsuccessful uploads
+      console.log("Upload Error: ", error.message)
+    }, 
+    () => {
+      // Handle successful uploads on complete
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log('File available at', downloadURL);
+        try {
+          // Setup updated user db record and update it
+          let payload = {
+            photoURL: downloadURL, 
+            photoPath: filePath
+          }
+          updateUserDBEntry(user, payload)
+          // Set state so site will rerender immediately
+          setUser({...user, ...payload})
+        } catch (e) {
+          console.log("Update Error: ", e.message)
+        }
+      });
+    }
+    );
+  }
+
+  const deleteProfilePictureUserFields = async () => {
+    try {
+      const userDbEntry = doc(db, "users", user.uid);
+  
+      await updateDoc(userDbEntry, {
+        photoPath: deleteField(),
+        photoURL: deleteField()
+      })
+
+      const updatedUser = {
+        ...user,
+        photoPath: null,
+        photoURL: null
+      }
+      setUser({...updatedUser});
+    } catch (error) {
+      console.log("Database entry deletion error: ", error.message);
+    }
+  }
+
+  const deleteProfilePicture = async () => {
+    try {
+      const storageRef = ref(storage, user.photoPath);
+
+      // Delete the file
+      deleteObject(storageRef).then(() => {
+        // TODO add success snackbar
+        console.log("Picture deletion successful")
+      }).catch((error) => {
+        // TODO add success snackbar
+        console.log("Picture deletion failed: ", error.message)
+      });
+    } catch (error) {
+      console.log("Delete file error: ", error.message);
+    }
+  }
+
+  const handleFileUpload = (e) => {
+    if (user.uid) {
+      uploadProfilePicture(e.target.files);
+    }
+  }
+
+  const handleFileDelete = () => {
+    deleteProfilePicture();
+    deleteProfilePictureUserFields();
+    handleClose();
   }
 
   const labelStyles = {
@@ -21,8 +152,52 @@ const ProfileInformation = () => {
 
   // TODO setup profile information form, submission, and firestore update
   return (
-      <Paper elevation={5}>
+      <Paper elevation={5} sx={{mb: "1em"}}>
         <Container maxWidth="lg">
+          <Divider sx={{pt: "1em"}}><Typography variant="h5" fontWeight="500">Profile</Typography></Divider>
+          <Grid container justifyContent={"space-between"} alignItems="center">
+            <Grid item>
+              <Typography variant="h6" sx={{pt: ".5em"}}>Profile Picture</Typography>
+            </Grid>
+            <Grid item>
+              <Button 
+                startIcon={<EditIcon />} 
+                onClick={handleEditProfileClick}
+                variant="outlined" 
+                size="small"
+              >
+                Edit
+              </Button>
+            </Grid>
+          </Grid>
+          <Divider />
+          {/* Profile Photo Section */}
+          <Grid container direction="row" alignItems={"center"} sx={{mt: "1em"}}>
+            <Grid item>
+              <Avatar 
+                sx={{height: "5em", width: "5em"}} 
+                alt={user?.firstName}
+                src={user.photoURL}
+              />
+            </Grid>
+            {(editProfilePhoto) ? (
+              <Grid item sx={{ml: "1em"}}>
+                <Button sx={{minWidth: "12em"}} variant="contained" component="label">
+                  Change Photo
+                  <input hidden accept="image/*" type="file" onChange={handleFileUpload}/>
+                </Button>
+                <br />
+                <Button sx={{minWidth: "12em", mt: ".5em"}} onClick={handleClickOpen} variant="outlined" color="error">
+                  Delete Photo
+                </Button>
+              </Grid>
+            ) : (
+              null
+            )}
+          </Grid>
+          <Typography variant="body2" sx={{mt: ".5em"}}>
+            Click <Link onClick={handleEditProfileClick} sx={{textDecoration: "none", fontWeight: "bold"}}>HERE</Link> to change your profile picture.</Typography>
+          {/* Profile Information Section */}
           <Grid container justifyContent={"space-between"} alignItems="center">
             <Grid item>
               <Typography variant="h6" sx={{pt: ".5em"}}>Profile Information</Typography>
@@ -30,7 +205,7 @@ const ProfileInformation = () => {
             <Grid item>
               <Button 
                 startIcon={<EditIcon />} 
-                onClick={handleEditClick}
+                onClick={handleEditProfileInformationClick}
                 variant="outlined" 
                 size="small"
               >
@@ -44,7 +219,7 @@ const ProfileInformation = () => {
               <Typography variant="body2" sx={labelStyles}>First Name</Typography>
             </Grid>
             <Grid item xs={9} sm={9} md={9}>
-              {(editProfile) ? (
+              {(editProfileInformation) ? (
                 <TextField
                   margin="dense"
                   id="firstname"
@@ -66,7 +241,7 @@ const ProfileInformation = () => {
               <Typography variant="body2"  sx={labelStyles}>Last Name</Typography>
             </Grid>
             <Grid item xs={9} sm={9} md={9}>
-              {(editProfile) ? (
+              {(editProfileInformation) ? (
                 <TextField
                   margin="dense"
                   id="lastname"
@@ -88,7 +263,7 @@ const ProfileInformation = () => {
               <Typography variant="body2"  sx={labelStyles}>Favorite Foods</Typography>
             </Grid>
             <Grid item xs={9} sm={9} md={9}>
-              {(editProfile) ? (
+              {(editProfileInformation) ? (
                 <TextField
                   margin="dense"
                   id="favoriteFoods"
@@ -109,7 +284,7 @@ const ProfileInformation = () => {
               <Typography variant="body2"  sx={labelStyles}>Food Allergies</Typography>
             </Grid>
             <Grid item xs={9} sm={9} md={9}>
-              {(editProfile) ? (
+              {(editProfileInformation) ? (
                 <TextField
                   margin="dense"
                   id="foodAllergies"
@@ -130,7 +305,7 @@ const ProfileInformation = () => {
               <Typography variant="body2"  sx={labelStyles}>Bio</Typography>
             </Grid>
             <Grid item xs={9} sm={9} md={9}>
-              {(editProfile) ? (
+              {(editProfileInformation) ? (
                 <TextField
                   margin="dense"
                   id="bio"
@@ -146,7 +321,41 @@ const ProfileInformation = () => {
               )}
             </Grid>
           </Grid>
+          {(editProfileInformation) ? (
+            <Button
+              onClick={handleSubmitProfileChanges}
+              startIcon={<SaveIcon />}
+              variant="contained"
+              size="small"
+              sx={{mb: "1.5em"}}
+            >
+              Update profile
+            </Button>
+          ) : (
+            null
+          )}
         </Container>
+        {/* DELETE PROFILE PICTURE DIALOG */}
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        >
+          <DialogTitle>
+            {"Delete user profile photo?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Once you delete your profile photo you won't be able to use the same photo unless you re-upload it.
+              Are you sure you wish to delete your profile photo?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleFileDelete}>
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Paper>
   )
 }
