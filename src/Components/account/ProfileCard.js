19fc64c68c6d961ae300fd370b31def1994a8bcc
Patrick Dolan <dolanp1992@gmail.com>
2022-09-14 11:00:29 -0700

    style: add 500 font weight to name display     style: add space between buttons and style format components

--- src/Components/account/ProfileCard.js
+++ src/Components/account/ProfileCard.js
@@ -16,14 +16,37 @@ const ProfileCard = (props) => {
               sx={{height: "5em", width: "5em", m: "auto", mb: ".5em"}} 
               src={user.photoURL}
             />
-            <Typography variant="subtitle1" align="center">{user?.firstName} {user?.lastName}</Typography>
+            <Typography 
+              variant="subtitle1" 
+              align="center" 
+              sx={{fontWeight: "500"}}
+            >
+              {user?.firstName} {user?.lastName}
+            </Typography>
             <Divider />
           </CardContent>
           {(toggleProfile) ? (
             <Box sx={{mx: ".5em", mb: ".5em"}}>
-              <Button onClick={() => toggleProfile(true)} startIcon={<AccountCircleIcon />} size="small" variant="outlined" fullWidth>Account</Button>
+              <Button 
+                onClick={() => toggleProfile(true)} 
+                startIcon={<AccountCircleIcon />} 
+                sx={{mb: ".5em"}}
+                size="small" 
+                variant="outlined" 
+                fullWidth
+              >
+                Account
+              </Button>
               <br />
-              <Button onClick={() => toggleProfile(false)} startIcon={<SettingsIcon />} size="small" variant="outlined" fullWidth>Settings</Button>
+              <Button 
+                onClick={() => toggleProfile(false)} 
+                startIcon={<SettingsIcon />} 
+                size="small" 
+                variant="outlined" 
+                fullWidth
+              >
+                Settings
+              </Button>
             </Box>
           ) : (
             null
