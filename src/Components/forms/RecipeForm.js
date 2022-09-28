36dc58a03f060e73b65c793044703357cccc463b
Patrick Dolan <dolanp1992@gmail.com>
2022-09-28 16:38:54 -0700

    refactor: change dropzone to dialog modal with preview image     feature: add functionality to upload recipe image and return url for display     update: add ingredients section to form and cook time     update: make name required     feature: add functionality for database document creation of recipe

--- src/Components/forms/RecipeForm.js
+++ src/Components/forms/RecipeForm.js
@@ -1,20 +1,40 @@
-import { Button, Divider, Grid, TextField, Typography } from "@mui/material";
+import { Button, Divider, Grid, TextField, Typography, MenuItem, Select, FormControl, InputLabel } from "@mui/material";
 import { Box, Container } from "@mui/system";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTheme } from "@emotion/react";
 import Dropzone from "./Dropzone";
+import { updateRecipeDBEntry } from "../../firebasefunctions";
+import { UserAuth } from "../../Contexts/AuthContext";
 
-const RecipeForm = () => {
+const RecipeForm = (props) => {
+  const { newRecipeId, setOpenCreate } = props;
   const [name, setName] = useState("New Recipe");
   const [description, setDescription] = useState("");
+  const [cooktime, setCooktime] = useState("");
   const [nutritionFacts, setNutritionFacts] = useState([]);
   const [instructions, setInstructions] = useState([{ step: 1, instruction: ""}])
-  const [files, setFiles] = useState([]);
+  const [ingredients, setIngredients] = useState([{ amount: 0, measurement: "", name: "", index: 0}])
+  const [openDropzone, setOpenDropzone] = useState(false);
+  const [recipeImageURL, setRecipeImageURL] = useState([]);
+  const [recipePreviewImage, setRecipePreviewImage] = useState([]);
+  const { user } = UserAuth();
+
+  useEffect(() => {
+    console.log(recipePreviewImage[0]);
+  }, [recipePreviewImage])
 
   const theme = useTheme();
 
+  const handleDropzoneClickOpen = () => {
+    setOpenDropzone(true);
+  };
+
   const handleAddInstructionTextField = () => {
     setInstructions([...instructions, {step: instructions.length + 1, instruction: ""}]);
+  }
+
+  const handleAddIngredientField = () => {
+    setIngredients([...ingredients, {name: "", amount: 0, measurement: "", index: ingredients.length}]);
   }
 
   const handleInstructionsChange = (e, index) => {
@@ -24,20 +44,56 @@ const RecipeForm = () => {
     setInstructions(updatedInstructions);
   }
 
+  const handleIngredientsValueChange = (e, index, value) => {
+    // Filter out old ingredient the add and sort new one in
+    const tempIngredients = ingredients.filter((a) => a.index !== index);
+    const updatedIngredients = [...tempIngredients, {...ingredients[index], [value]: e.target.value.trim()}].sort((a, b) => a.index > b.index ? 1 : -1);
+    setIngredients(updatedIngredients);
+  }
+
+  console.log(ingredients)
+  
   const handleNutritionChange = (factName, e) => {
+    // Filter out old nutrition fact then add new one in
     const filteredNutritionFacts = nutritionFacts.filter((a) => a.name !== factName);
     setNutritionFacts([...filteredNutritionFacts, { name: factName, value: e.target.value }]);
   }
 
+  // Takes in boolean indicating if its a draft or not
+  const uploadRecipe = async (isDraft) => {
+    // Create Recipe Obj
+    const recipe = {
+      name: name,
+      id: newRecipeId,
+      description: description,
+      cooktime: cooktime,
+      nutritionFacts: nutritionFacts,
+      instructions: instructions,
+      ingredients: ingredients,
+      isDraft: isDraft,
+      isPublic: !isDraft,
+      thumbnail_url: recipeImageURL || "https://bit.ly/3dSiUZK",
+      thumbnial_alt_text: name || "New Recipe"
+    }
+
+    // Upload to firestore
+    try {
+      await updateRecipeDBEntry(user, recipe);
+    } catch (error) {
+      // TODO add snackbar for success and fail
+      console.log(error.message);
+    }
+  }
+
   const handleSaveDraft = () => {
-    alert("Save clicked")
+    uploadRecipe(true);
+    setOpenCreate(false);
   }
 
   const handlePublishRecipe = () => {
-    alert("Publish clicked")
+    uploadRecipe(false);
+    setOpenCreate(false);
   }
-
-  console.log(nutritionFacts)
 
   return (
     <Container sx={{mb: "5em"}}>
@@ -53,6 +109,7 @@ const RecipeForm = () => {
         <Grid item xs={12} sm={12} md={12}>
           <TextField
             fullWidth
+            required
             name="name"
             label="Recipe Name"
             variant="outlined"
@@ -70,18 +127,23 @@ const RecipeForm = () => {
             onChange={(e) => setDescription(e.target.value)}
           />
         </Grid>
-      </Grid>
-      <Typography variant="h5" sx={{ mt: "1em"}}>Recipe photo</Typography>
-
-      <Grid container spacing={2} textAlign="center">
-        <Grid item xs={12} sm={6} md={6}>
-          <Dropzone
-            files={files}
-            setFiles={setFiles}
+        <Grid item xs={12} sm={12} md={12}>
+          <TextField
+            fullWidth
+            type="number"
+            name="cooktime"
+            label="Cook Time"
+            helperText="Cook time is measured in minutes."
+            variant="outlined"
+            onChange={(e) => setCooktime(e.target.value)}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={6}>
-          {(files[0]?.preview) ? (
+      </Grid>
+      <Typography variant="h5" sx={{ mt: "1em"}}>Recipe photo</Typography>
+      <Typography variant="subtitle2" color={theme.palette.text.secondary} sx={{mb: ".5em"}}>Image types accepted include jpeg, jpg, and png.</Typography>
+      <Grid container spacing={2} textAlign="center">
+        <Grid item xs={12} sm={12} md={12}>
+          {(recipePreviewImage[0]?.preview) ? (
               <Box 
                 sx={{
                   border: "1px solid black",
@@ -91,12 +153,32 @@ const RecipeForm = () => {
                   flexDirection:"column"
                 }}
               >
-                <Box sx={{mx :"auto", my: "1em"}}>
-                  <img src={files[0].preview} style={{width: "10em"}} alt="Recipe preview" />
+                <Box sx={{mx :"auto", py: "1em"}}>
+                  <img src={recipePreviewImage[0].preview} style={{width: "10em"}} alt="Recipe preview" />
                 </Box>
-                <Typography variant="h5">Image Preview</Typography>
+                <Typography variant="h5">Recipe Image Preview</Typography>
+                <Button 
+                  onClick={handleDropzoneClickOpen}
+                  variant="outlined"
+                  sx= {{
+                    maxWidth: "10em",
+                    m: "auto",
+                    mb: "1em"
+                  }}
+                >
+                  Change Image
+                </Button>
+                <Dropzone
+                  setRecipePreviewImage={setRecipePreviewImage}
+                  openDropzone={openDropzone}
+                  setOpenDropzone={setOpenDropzone}
+                  newRecipeId={newRecipeId}
+                  user={user}
+                  setRecipeImageURL={setRecipeImageURL}
+                />
               </Box>
           ) : (
+            <>
               <Box 
                 sx={{
                   border: "1px solid black",
@@ -106,13 +188,33 @@ const RecipeForm = () => {
                   flexDirection:"column"
                 }}
               >
-                <Typography variant="h5">Image preview</Typography>
+                <Typography variant="h5" sx={{py: "3em"}}>Recipe Image preview</Typography>
+              <Button 
+                onClick={handleDropzoneClickOpen}
+                variant="outlined"
+                sx= {{
+                  maxWidth: "10em",
+                  m: "auto",
+                  mb: "1em"
+                }}
+              >
+                Upload Image
+              </Button>
               </Box>
+              <Dropzone
+                setRecipePreviewImage={setRecipePreviewImage}
+                openDropzone={openDropzone}
+                setOpenDropzone={setOpenDropzone}
+                newRecipeId={newRecipeId}
+                user={user}
+                setRecipeImageURL={setRecipeImageURL}
+              />
+            </>
           )}
         </Grid>
       </Grid>
       <Typography variant="h5" sx={{ mt: "1em"}}>Nutrition Facts</Typography>
-      <Typography variant="subtitle2" color={theme.palette.text.secondary}>All Nutrional information is saved in grams.</Typography>
+      <Typography variant="subtitle2" color={theme.palette.text.secondary}>All Nutrional information, other than calories, is saved in grams.</Typography>
       <Grid container sx={{ mt: ".5em"}}>
         <Grid container item spacing={2} >
           <Grid item xs={12} sm={6} md={6}>
@@ -166,6 +268,78 @@ const RecipeForm = () => {
               variant="outlined"
               onChange={(e) => handleNutritionChange("Protien", e)}
             />
+          </Grid>
+        </Grid>
+        <Grid container item>
+          <Grid item xs={12} sm={12} md={12}>
+            <Typography variant="h5" sx={{ mt: "1em"}}>Ingredients</Typography>
+            <Typography variant="subtitle2" color={theme.palette.text.secondary}>You can add up to 50 ingredients.</Typography>
+            {ingredients.map((ingredient) => 
+              <Box key={ingredient.index}>
+                <Grid container item xs={12} sm={12} md={12} spacing={1}>
+                  <Grid item xs={12} sm={2} md={2}>
+                    <FormControl fullWidth sx={{ mt: "1em"}} >
+                      <TextField
+                        fullWidth
+                        type="number"
+                        size="small"
+                        InputProps={{ inputProps: {min: 0}}}
+                        key={ingredient.index}
+                        label={"Amount"}
+                        variant="outlined"
+                        onChange={(e) => handleIngredientsValueChange(e, ingredient.index, "amount")}
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={3} md={3}>
+                    <FormControl fullWidth sx={{ mt: "1em"}} size="small">
+                      <InputLabel id="ingredientMeasurement">Measurement</InputLabel>
+                      <Select
+                        value={ingredient.measurement}
+                        label="Measurement"
+                        
+                        onChange={(e) => handleIngredientsValueChange(e, ingredient.index, "measurement")}
+                      >
+                        <MenuItem value={"Tablespoon(s)"}>Tablespoon(s)</MenuItem>
+                        <MenuItem value={"Teaspoon(s)"}>Teaspoon(s)</MenuItem>
+                        <MenuItem value={"Ounce(s)"}>Ounce(s)</MenuItem>
+                        <MenuItem value={"Fluid ounce(s)"}>Fluid ounce(s)</MenuItem>
+                        <MenuItem value={"Cup(s)"}>Cup(s)</MenuItem>
+                        <MenuItem value={"Quart(s)"}>Quart(s)</MenuItem>
+                        <MenuItem value={"Pint(s)"}>Pint(s)</MenuItem>
+                        <MenuItem value={"Gallon(s)"}>Gallon(s)</MenuItem>
+                        <MenuItem value={"Pound(s)"}>Pound(s)</MenuItem>
+                        <MenuItem value={"Milliliter(s)"}>Milliliter(s)</MenuItem>
+                        <MenuItem value={"Gram(s)"}>Gram(s)</MenuItem>
+                        <MenuItem value={"Kilogram(s)"}>Kilogram(s)</MenuItem>
+                        <MenuItem value={"Liter(s)"}>Liter(s)</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={7} md={7}>
+                    <FormControl fullWidth sx={{ mt: "1em"}}>
+                      <TextField
+                        fullWidth
+                        size="small"
+                        key={ingredient.index}
+                        label={"Ingredient " + (ingredient.index + 1)}
+                        variant="outlined"
+                        onChange={(e) => handleIngredientsValueChange(e, ingredient.index, "name")}
+                      />
+                    </FormControl>
+                  </Grid>
+                </Grid>
+              </Box>
+            )}
+            {(ingredients.length >= 50) ? (null) : (
+              <Button
+                onClick={handleAddIngredientField}
+                variant="outlined"
+                sx={{mt: ".5em"}}
+              >
+                Add another ingredient
+              </Button>
+            )}
           </Grid>
         </Grid>
         <Grid container item>
