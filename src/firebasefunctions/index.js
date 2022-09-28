9333571b7fb631c55c208065e592684885f3af8b
Patrick Dolan <dolanp1992@gmail.com>
2022-09-27 17:37:02 -0700

    update: add create update and get functionality for recipes in database     update: add comments and seperate logic for user creation and recipe

--- src/firebasefunctions/index.js
+++ src/firebasefunctions/index.js
@@ -1,7 +1,10 @@
 import { db } from "../firebase";
-import { doc, setDoc, serverTimestamp, getDoc } from "firebase/firestore";
+import { doc, setDoc, serverTimestamp, getDoc, addDoc, collection, getDocs } from "firebase/firestore";
+import { v4 } from "uuid";
 
 // ==================== Firestore Functions ====================
+
+// ====== User Functions ======
 
 // Add or Update takes in user from auth and userDetails object
 export const updateUserDBEntry = async (user, userDetails) => {
@@ -19,11 +22,51 @@ export const getUserData = async (user) => {
   if (user?.uid) {
     const docRef = doc(db, "users", user.uid);
     const docSnap = await getDoc(docRef);
-  
+    
     if (docSnap.exists()) {
       return docSnap.data();
     } else {
       console.log("No document found.");
     }
+  }
+}
+
+// ====== Recipe Functions ======
+
+// Add takes in user and recipe object to create/update entry
+export const createRecipeDBEntry = async (user) => {
+  // Create recipes collection and add new document to it as empty recipe with created at date
+  const collectionRef = collection(db, "users", user.uid, "recipes")
+  const docRef = await addDoc(collectionRef, {
+    key: v4(),
+    isDraft: true,
+    isPublic: false,
+    createdAt: serverTimestamp()
+  });
+
+  return docRef.id;
+}
+
+// Update takes in user and recipe to update record in DB
+export const updateRecipeDBEntry = async (user, recipe) => {
+  // Create Account doc for new user with same user id
+  const docRef = doc(db, "users", user.uid, "recipes", recipe.id );
+  const payload = {
+    ...recipe,
+    createdAt: recipe.createdAt || serverTimestamp()
+  }
+  await setDoc(docRef, payload, { merge: true });
+}
+
+// Get User recipes
+export const getUserRecipes = async (user) => {
+  if (user?.uid) {
+    const recipes = [];
+    const collectionRef = collection(db, "users", user.uid, "recipes");
+    const querySnapshot = await getDocs(collectionRef);
+    querySnapshot.forEach((doc) => {
+      recipes.push(doc.data());
+    });
+    return recipes;
   }
 }
